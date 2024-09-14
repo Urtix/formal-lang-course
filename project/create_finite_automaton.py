c62d7b5a5d9f8cc0492fe7ffd1815fb0a3300929
Urtix <es.perevalov@mail.ru>
2024-09-15 01:41:15 +0300

    feat: Implement the function of constructing a non-deterministic finite automaton from a graph

--- project/create_finite_automaton.py
+++ project/create_finite_automaton.py
@@ -7,3 +7,24 @@ def regex_to_dfa(regex: str) -> fa.DeterministicFiniteAutomaton:
     dfa = nfa.to_deterministic()
 
     return dfa.minimize()
+
+
+def graph_to_nfa(
+    graph: MultiDiGraph, start_states: set[int], final_states: set[int]
+) -> fa.NondeterministicFiniteAutomaton:
+    enfa = fa.NondeterministicFiniteAutomaton.from_networkx(graph)
+    nfa = enfa.remove_epsilon_transitions()
+
+    if not start_states:
+        start_states = set(int(n) for n in graph.nodes)
+
+    if not final_states:
+        final_states = set(int(n) for n in graph.nodes)
+
+    for s in start_states:
+        nfa.add_start_state(s)
+
+    for f in final_states:
+        nfa.add_final_state(f)
+
+    return nfa
