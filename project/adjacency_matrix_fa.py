0f04c57a75fd5d7df3f257ad15da759a1a6bb1f8
Urtix <es.perevalov@mail.ru>
2024-09-23 23:15:46 +0300

    refactor: Renaming a variable

--- project/adjacency_matrix_fa.py
+++ project/adjacency_matrix_fa.py
@@ -13,7 +13,7 @@ class AdjacencyMatrixFA:
             self.states = dict()
             self.start_states = set()
             self.final_states = set()
-            self.matrix = dict()
+            self.decomposition = dict()
             return
 
         graph = automaton.to_networkx()
@@ -34,7 +34,7 @@ class AdjacencyMatrixFA:
                 state2 = self.states[s2]
                 transitions[label][state1, state2] = 1
 
-        self.matrix = dict(
+        self.decomposition = dict(
             zip(
                 transitions.keys(),
                 [csr_matrix(matrix) for matrix in transitions.values()],
@@ -45,11 +45,11 @@ class AdjacencyMatrixFA:
         states = set(self.start_states)
 
         for letter in word:
-            if self.matrix.get(letter) is None:
+            if self.decomposition.get(letter) is None:
                 return False
 
             for s1, s2 in product(states, self.states.keys()):
-                if self.matrix[letter][self.states[s1], self.states[s2]]:
+                if self.decomposition[letter][self.states[s1], self.states[s2]]:
                     states.add(s2)
 
         if states.intersection(self.final_states):
@@ -60,8 +60,8 @@ class AdjacencyMatrixFA:
     def transitive_сlosure(self) -> np.ndarray:
         A = np.eye(self.number_of_states, dtype=bool)
 
-        for matrix in self.matrix.values():
-            A |= matrix.toarray()
+        for dec in self.decomposition.values():
+            A |= dec.toarray()
 
         transitive_сlosure = np.linalg.matrix_power(A, self.number_of_states).astype(
             bool
@@ -105,14 +105,14 @@ def intersect_automata(
         intersection_matrix.states.keys()
     ).intersection(product(automaton1.final_states, automaton2.final_states))
 
-    intersection_matrix.matrix = {
+    intersection_matrix.decomposition = {
         key: kron(
-            automaton1.matrix[key],
-            automaton2.matrix[key],
+            automaton1.decomposition[key],
+            automaton2.decomposition[key],
             format="csr",
         )
-        for key in automaton1.matrix.keys()
-        if key in automaton2.matrix
+        for key in automaton1.decomposition.keys()
+        if key in automaton2.decomposition
     }
 
     return intersection_matrix
