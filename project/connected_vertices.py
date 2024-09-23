e3e55ce5d1b56f82d7024dde8751c58b12d9ee31
Urtix <es.perevalov@mail.ru>
2024-09-23 22:51:53 +0300

    feat: Implement a function for finding connected vertices

---
+++ project/connected_vertices.py
@@ -0,0 +1,32 @@
+import networkx as nx
+
+from itertools import product
+
+from project.adjacency_matrix_fa import AdjacencyMatrixFA, intersect_automata
+from project.create_finite_automaton import regex_to_dfa, graph_to_nfa
+
+
+def tensor_based_rpq(
+    regex: str,
+    graph: nx.MultiDiGraph,
+    start_nodes: set[int] = None,
+    final_nodes: set[int] = None,
+) -> set[tuple[int, int]]:
+    regex_to_matrix = AdjacencyMatrixFA(regex_to_dfa(regex))
+    graph_to_matrix = AdjacencyMatrixFA(graph_to_nfa(graph, start_nodes, final_nodes))
+    intersection = intersect_automata(regex_to_matrix, graph_to_matrix)
+    closure = intersection.transitive_сlosure()
+
+    return {
+        (graph_start, graph_final)
+        for graph_start in graph_to_matrix.start_states
+        for graph_final in graph_to_matrix.final_states
+        if any(
+            closure[
+                intersection.states[(regex_start, graph_start)],
+                intersection.states[(regex_final, graph_final)],
+            ]
+            for regex_start in regex_to_matrix.start_states
+            for regex_final in regex_to_matrix.final_states
+        )
+    }
